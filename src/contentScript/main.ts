7b730956b192c0ca45b57dee3c12f5286325be16
Cside <cside.story@gmail.com>
2023-09-25 19:16:34 +0900

    fix for empty title memo

--- src/contentScript/main.ts
+++ src/contentScript/main.ts
@@ -2,26 +2,27 @@ const serviceName = 'Google Keep';
 
 const main = () => {
   if (['#home', '#search'].some((hash) => location.hash === hash)) {
+    document.title = '';
+  } else if (location.hash.startsWith('#label/')) {
+    document.title = decodeURIComponent(location.hash).replace(/^#label\//, '');
+  } else if (location.hash.startsWith('#NOTE/')) {
+    document.title =
+      document.querySelector<HTMLElement>('[contenteditable=true][spellcheck=true]')?.textContent ||
+      '';
+  } else if (location.hash.startsWith('#search/text')) {
+    // Use value in input element because the query in location.hash includes commas
+    const $input = document.querySelector<HTMLInputElement>('input[name="q"]');
+    if (!$input) throw new Error('input[name="q"] is not found');
+    document.title = $input.value;
+  } else {
+    const $heading = document.querySelector('[role="heading"]');
+    if (!$heading) throw new Error('[role="heading"] is not found');
+    document.title =
+      $heading.textContent !== null && $heading.textContent.length > 0 ? $heading.textContent : '';
+  }
+  if (document.title === '') {
     document.title = serviceName;
   } else {
-    if (location.hash.startsWith('#label/')) {
-      document.title = decodeURIComponent(location.hash).replace(/^#label\//, '');
-    } else if (location.hash.startsWith('#NOTE/')) {
-      const title =
-        document.querySelector<HTMLElement>('[contenteditable=true][spellcheck=true]')
-          ?.textContent || serviceName;
-      if (title) document.title = title;
-    } else if (location.hash.startsWith('#search/text')) {
-      const $input = document.querySelector<HTMLInputElement>('input[name="q"]');
-      if (!$input) throw new Error('input[name="q"] is not found');
-      document.title = $input.value;
-    } else {
-      // query in location.hash includes commas
-      const $heading = document.querySelector('[role="heading"]');
-      if (!$heading) throw new Error('[role="heading"] is not found');
-      if ($heading.textContent === null || $heading.textContent?.length === 0) return;
-      document.title = $heading.textContent;
-    }
     document.title += ` - ${serviceName}`;
   }
 };
